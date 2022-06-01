bf3f441887c56993835828efe336f6e2fd53e76a
Jake Burton <jburto94@yahoo.com>
2022-06-01 12:46:15 -0400

    phonebook can add a new name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const handleNewName = e => {
+    setNewName(e.target.value);
+  }
+
+  const addNewPerson = e => {
+    e.preventDefault();
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  }
+
   return (
     <div>
-
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input onChange={handleNewName} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
     </div>
   );
 };
